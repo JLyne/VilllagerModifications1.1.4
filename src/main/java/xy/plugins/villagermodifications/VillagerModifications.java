8d8d2ece3326f9d0eddb1daccc86aee5823db908
James Lyne <jim+github@not-null.co.uk>
2021-02-01 23:22:26 +0000

    Regenerate prices of altered enchanted books to match vanilla values for the new enchantment

--- src/main/java/xy/plugins/villagermodifications/VillagerModifications.java
+++ src/main/java/xy/plugins/villagermodifications/VillagerModifications.java
@@ -69,7 +69,6 @@ public final class VillagerModifications extends JavaPlugin implements Listener 
                 Enchantment enchantment = Enchantment.getByKey(NamespacedKey.minecraft(key));
 
                 if(enchantment != null) {
-                    getLogger().info(key + ": " + minVillagerLevels.getInt(key, 1));
                     this.minEnchantLevels.put(enchantment, minVillagerLevels.getInt(key, 1));
                 } else {
                     getLogger().warning("Invalid enchantment " + key);
@@ -285,11 +284,14 @@ public final class VillagerModifications extends JavaPlugin implements Listener 
 
         int pos = -1;
         for (MerchantRecipe recipe : villager.getRecipes()) {
-            boolean changed = false;
+            boolean changed = false; //Whether any enchantments were modified
+            int highestLevel = 1; //Highest level of any enchantment in the result item, used for generating price
+            boolean isTreasure = false; //Whether any enchantments are treasure, used for generating price
+
             pos++;
 
             if(pos <= lastCheckedIndex) {
-                getLogger().info("Skpping recipe " + pos + " as already checked before");
+                getLogger().info("Skipping recipe " + pos + " as already checked before");
                 continue;
             }
 
@@ -300,56 +302,74 @@ public final class VillagerModifications extends JavaPlugin implements Listener 
             }
 
             EnchantmentStorageMeta meta = (EnchantmentStorageMeta) result.getItemMeta();
-            getLogger().info("--------------------------------------");
-            getLogger().info("Checking book trade " + meta.toString());
 
             for (Enchantment enchantment : meta.getStoredEnchants().keySet()) {
                 int level = meta.getStoredEnchantLevel(enchantment);
-                getLogger().info("Checking enchant: " + enchantment.toString() + ":" + level);
 
                 //Enchantment type isn't allowed, replace with another
                 if(!isEnchantmentAllowed(enchantment, villagerLevel, disallowed)) {
-                    getLogger().info("Enchantment not allowed. Replacing");
                     meta.removeStoredEnchant(enchantment);
 
+                    //Get allowed enchantment and level
                     Enchantment replacement = getEnchantment(villagerLevel, disallowed);
                     List<Integer> range = getEnchantmentLevelRange(replacement, villagerLevel);
                     int replacementLevel = range.get(random.nextInt(range.size()));
 
-                    getLogger().info("Replaced with: " + replacement.toString() + ":" + replacementLevel);
+                    getLogger().info("Replaced " + enchantment.getKey().getKey() + ":" + level + " with: " + replacement.getKey().getKey() + ":" + replacementLevel);
 
                     disallowed.add(replacement);
                     meta.addStoredEnchant(replacement, replacementLevel, false);
+
                     changed = true;
+                    isTreasure = isTreasure || replacement.isTreasure();
+                    highestLevel = Math.max(highestLevel, replacementLevel);
                 } else {
                     List<Integer> allowedLevels = getEnchantmentLevelRange(enchantment, villagerLevel);
 
                     //Enchantment level isn't allowed, change level
                     if(!allowedLevels.contains(level)) {
-                        getLogger().info("Level " + level + " not allowed. Replacing");
                         int replacementLevel = allowedLevels.get(random.nextInt(allowedLevels.size()));
 
-                        getLogger().info("Replaced with: " + replacementLevel);
+                        getLogger().info("Replaced " + enchantment + ":" + level + " with level: " + replacementLevel);
                         meta.removeStoredEnchant(enchantment);
                         meta.addStoredEnchant(enchantment, replacementLevel, false);
+
                         changed = true;
-                    } else {
-                        getLogger().info("Everything is cool");
+                        highestLevel = Math.max(highestLevel, replacementLevel);
                     }
 
                     disallowed.add(enchantment);
+                    isTreasure = isTreasure || enchantment.isTreasure();
                 }
             }
 
             if(changed) {
                 result.setItemMeta(meta);
+
+                //Copy recipe so we can change the result item
                 MerchantRecipe newRecipe = new MerchantRecipe(result, recipe.getMaxUses());
+                List<ItemStack> ingredients = recipe.getIngredients();
+                ItemStack firstItem = ingredients.get(0).clone();
+                ItemStack secondItem = ingredients.get(1);
+
+                //Generate new price to reflect level/type changes
+                if(firstItem.getType() == Material.EMERALD) {
+                    int price = getEnchantmentPrice(highestLevel, isTreasure);
+                    getLogger().info("Changing price to: " + price);
+                    firstItem.setAmount(price);
+                }
 
                 newRecipe.setUses(recipe.getUses());
                 newRecipe.setPriceMultiplier(recipe.getPriceMultiplier());
                 newRecipe.setExperienceReward(recipe.hasExperienceReward());
                 newRecipe.setVillagerExperience(recipe.getVillagerExperience());
-                newRecipe.setIngredients(recipe.getIngredients());
+                newRecipe.addIngredient(firstItem);
+
+                if(secondItem != null) {
+                    newRecipe.addIngredient(secondItem);
+                }
+
+                getLogger().info(newRecipe.toString());
 
                 villager.setRecipe(pos, newRecipe);
             }
@@ -382,13 +402,11 @@ public final class VillagerModifications extends JavaPlugin implements Listener 
         int minVillagerLevel = config.getInt("enchantments.min-villager-levels." + enchantment.getKey().getKey(), 1);
 
         if(villagerLevel < minVillagerLevel) {
-            getLogger().info(range.toString());
             return range;
         }
 
         if(enchantment.getMaxLevel() == 1) {
             range.add(1);
-            getLogger().info(range.toString());
             return range;
         }
 
@@ -403,7 +421,6 @@ public final class VillagerModifications extends JavaPlugin implements Listener 
         //and current villager level is the min required for this enchantment
         //limit range to lowest level
         if(enchantment.getMaxLevel() <= (4 - minVillagerLevel) && villagerLevel == minVillagerLevel) {
-            getLogger().info(range.toString());
             return range;
         }
 
@@ -416,9 +433,37 @@ public final class VillagerModifications extends JavaPlugin implements Listener 
             range.add(baseLevel + 1);
         }
 
-        getLogger().info(range.toString());
-
         return range;
+    }
+
+    public int getEnchantmentPrice(int level, boolean treasure) {
+        int price;
+
+        switch(level) {
+            case 1:
+            default:
+                price = random.nextInt(15) + 5; //5-19
+                break;
+            case 2:
+                price = random.nextInt(25) + 8; //8-32
+                break;
+            case 3:
+                price = random.nextInt(35) + 11; //11-45
+                break;
+            case 4:
+                price = random.nextInt(45) + 14; //14-58
+                break;
+            case 5:
+                price = random.nextInt(55) + 17; //17-71
+                break;
+
+        }
+
+        if(treasure) {
+            price = price * 2;
+        }
+
+        return price;
     }
 
     @Override
